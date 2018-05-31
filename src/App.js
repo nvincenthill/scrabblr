d5b60cbc2c9a227de7731a13a34c5f98c8e493b2
mypersonalgithubaccount <nvincenthill@gmail.com>
2018-05-31 08:16:20 -0700

    Hack together react dnd and react flip move implementation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import React, { Component } from "react";
+import React, { Component, PropTypes } from "react";
 import "./App.css";
-let checkWord = require("check-word");
+import Dictionary from "./Dictionary.json";
+import Scrabble from "./Scrabble.js";
+
+import moment from "moment";
+import { times } from "lodash";
+import classNames from "classnames";
+import HTML5Backend from "react-dnd-html5-backend";
+import { DragSource, DropTarget, DragDropContext } from "react-dnd";
+import FlipMove from "react-flip-move";
+import Toggle from "./Toggle.js";
+import helpers from "./helpers.js";
 
 // first we will make a new context
 const MyContext = React.createContext();
@@ -11,6 +21,7 @@ class MyProvider extends Component {
     super(props);
     this.state = {
       word: "example",
+      randomWord: "",
       alphabet: [
         "a",
         "b",
@@ -38,37 +49,130 @@ class MyProvider extends Component {
         "x",
         "y",
         "z"
+      ],
+      tiles: [
+        { id: 1, letter: "F", points: 4, x: 0, y: 3 },
+        { id: 2, letter: "L", points: 2, x: 1, y: 3 },
+        { id: 3, letter: "I", points: 1, x: 2, y: 3 },
+        { id: 4, letter: "P", points: 2, x: 3, y: 3 },
+        { id: 5, letter: "M", points: 6, x: 4, y: 3 },
+        { id: 6, letter: "O", points: 1, x: 5, y: 3 },
+        { id: 7, letter: "V", points: 8, x: 6, y: 3 },
+        { id: 8, letter: "E", points: 2, x: 7, y: 3 },
+        { id: 9, letter: "E", points: 2, x: 8, y: 3 },
+        { id: 10, letter: "E", points: 2, x: 9, y: 3 }
       ]
     };
   }
 
   // generate a random word of length n
   getRandomWord = (arr, n) => {
-    let shuffledArray = this.shuffleArray(arr);
-    console.log(shuffledArray.slice(0, n).join(''));
-    return shuffledArray.slice(0, n).join('');
+    let randomWord = [];
+    for (let i = 0; i < n; i++) {
+      let randomLetter = this.shuffleArray(arr)[0]
+      randomWord.push(randomLetter);
+      let temp = this.state.tiles;
+      temp[i].letter = randomLetter; 
+      this.setState({tiles: temp})
+    }
+    randomWord = randomWord.join("");
+    return randomWord;
+  };
+
+  generateRandomWord = () => {
+    let word = this.getRandomWord(this.state.alphabet, 10);
+    this.save(word);
+  };
+
+  generateMatches = () => {
+    let allPosible = this.permute__of_all_size(this.state.randomWord);
+    let result = [];
+    for (let i = 0; i < allPosible.length; i++) {
+      if (Dictionary.hasOwnProperty(allPosible[i])) {
+        result.push(allPosible[i]);
+      }
+    }
+    result = result.reverse();
+    console.log(result);
+    return result;
+  };
+
+  //setState
+  save = data => {
+    this.setState({ randomWord: data });
   };
 
   //Durstenfeld shuffle
   shuffleArray = array => {
-    for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
       array[i] = array[j];
       array[j] = temp;
     }
     return array;
   };
 
+  //Find all permutations of an array
+  swap = (array, i, j) => {
+    if (i !== j) {
+      let swap = array[i];
+      array[i] = array[j];
+      array[j] = swap;
+    }
+  };
+
+  permute_rec = (res, str, array) => {
+    if (array.length === 0) {
+      res.push(str);
+    } else {
+      for (let i = 0; i < array.length; i++) {
+        this.swap(array, 0, i);
+        this.permute_rec(res, str + array[0], array.slice(1));
+        this.swap(array, 0, i);
+      }
+    }
+  };
+
+  permute = array => {
+    let res = [];
+    this.permute_rec(res, "", array);
+    return res;
+  };
+
+  xpermute_rec = (res, sub, array) => {
+    if (array.length === 0) {
+      if (sub.length > 0) this.permute_rec(res, "", sub);
+    } else {
+      this.xpermute_rec(res, sub, array.slice(1));
+      this.xpermute_rec(res, sub.concat(array[0]), array.slice(1));
+    }
+  };
+
+  permute__of_all_size = array => {
+    let res = [];
+    this.xpermute_rec(res, [], array);
+    return res;
+  };
+
+  updateTiles = (stateTiles) => {
+    this.setState({ tiles: stateTiles });
+  }
+
+  componentWillMount() {
+
+  }
+
   render() {
     return (
       <MyContext.Provider
-        value={
-          {
-            getRandomWord: this.getRandomWord,
-            state: this.state
-          }
-        }
+        value={{
+          getRandomWord: this.getRandomWord,
+          generateRandomWord: this.generateRandomWord,
+          generateMatches: this.generateMatches,
+          state: this.state,
+          updateTiles: this.updateTiles
+        }}
       >
         {this.props.children}
       </MyContext.Provider>
@@ -84,11 +188,14 @@ class App extends Component {
           {context => (
             <div className="App">
               <header className="App-header">
-                <h1 className="App-title">WordFlipper</h1>
+                <h1 className="App-title">WordFlippr</h1>
               </header>
-              <p className="App-intro">
-                {checkWord("en") ? context.getRandomWord(context.state.alphabet, 10) : "FAIL"}
-              </p>
+              <p className="App-intro">{context.state.randomWord}</p>
+              <button onClick={context.generateRandomWord}>
+                Generate Word
+              </button>
+              <button onClick={context.generateMatches}>Generate Matchs</button>
+              <Scrabble tiles={context.state.tiles} updateTiles={context.updateTiles}/>
             </div>
           )}
         </MyContext.Consumer>
@@ -98,3 +205,6 @@ class App extends Component {
 }
 
 export default App;
+
+// export context
+export { MyContext };
---
+++ src/Scrabble.js
@@ -0,0 +1,194 @@
+import React, { Component, PropTypes } from "react";
+import moment from "moment";
+import { times } from "lodash";
+import classNames from "classnames";
+import HTML5Backend from "react-dnd-html5-backend";
+import { DragSource, DropTarget, DragDropContext } from "react-dnd";
+import FlipMove from "react-flip-move";
+import Toggle from "./Toggle.js";
+import helpers from "./helpers.js";
+import { MyContext } from "./App.js";
+
+const BOARD_WIDTH = 10;
+const BOARD_HEIGHT = 7;
+const SQUARE_SIZE = 56;
+const TILE_OFFSET = 3;
+const NUM_SQUARES = BOARD_WIDTH * BOARD_HEIGHT;
+
+@DragDropContext(HTML5Backend)
+class Scrabble extends Component {
+  constructor() {
+    super();
+
+    this.updateDroppedTilePosition = this.updateDroppedTilePosition.bind(this);
+    this.resetTiles = this.resetTiles.bind(this);
+  }
+
+  updateDroppedTilePosition({ x, y }, tile) {
+    // Normally, this would be done through a Redux action, but because this
+    // is such a contrived example, I'm just passing the action down through
+    // the child.
+
+    // Create a copy of the state, find the newly-dropped tile.
+    let stateTiles = this.props.tiles.slice();
+    const index = stateTiles.findIndex(stateTile => stateTile.id === tile.id);
+
+    // Set it to a new copy of the tile, but with the new coords
+    stateTiles[index] = { ...tile, x, y };
+
+    this.props.updateTiles(stateTiles);
+  }
+
+  resetTiles() {
+    // this.setState({ tiles });
+  }
+
+  renderTiles() {
+    return this.props.tiles.map((tile, index) => {
+      return (
+        <Tile key={index} onDrop={this.updateDroppedTilePosition} {...tile} />
+      );
+    });
+  }
+
+  renderBoardSquares() {
+    // Create a 2D array to represent the board
+    // Array#matrix is a monkeypatched, custom method >:)
+    const matrix = Array.matrix(BOARD_WIDTH, BOARD_HEIGHT);
+
+    return matrix.map((row, rowIndex) =>
+      row.map(index => {
+        return (
+          <BoardSquare
+            x={index}
+            y={rowIndex}
+            onDrop={this.updateDroppedTilePosition}
+          />
+        );
+      })
+    );
+  }
+
+  render() {
+    return (
+      <MyContext.Consumer>
+        {context => (
+          <div id="scrabble">
+            <div className="board-border">
+              <div className="board">
+                <FlipMove duration={200} staggerDelayBy={150}>
+                  {this.renderTiles()}
+                </FlipMove>
+                {this.renderBoardSquares()}
+              </div>
+            </div>
+
+            <div className="controls">
+              <Toggle
+                clickHandler={this.resetTiles}
+                text="Reset"
+                icon="refresh"
+                active={true}
+                large={true}
+              />
+            </div>
+          </div>
+        )}
+      </MyContext.Consumer>
+    );
+  }
+}
+
+const tileSource = {
+  beginDrag(props) {
+    return props;
+  }
+};
+
+const tileTarget = {
+  drop(props, monitor) {
+    const tile1 = props;
+    const tile2 = monitor.getItem();
+
+    props.onDrop(tile1, tile2);
+    props.onDrop(tile2, tile1);
+  }
+};
+
+@DropTarget("tile", tileTarget, (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  isOver: monitor.isOver()
+}))
+@DragSource("tile", tileSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+}))
+class Tile extends Component {
+  static propTypes = {
+    // x: PropTypes.number.isRequired,
+    // y: PropTypes.number.isRequired,
+    // letter: PropTypes.string.isRequired,
+    // points: PropTypes.number.isRequired,
+    // connectDragSource: PropTypes.func.isRequired,
+    // isDragging: PropTypes.bool.isRequired
+  };
+
+  render() {
+    const {
+      connectDropTarget,
+      connectDragSource,
+      isDragging,
+      letter,
+      points,
+      x,
+      y
+    } = this.props;
+
+    const styles = {
+      left: x * SQUARE_SIZE - TILE_OFFSET,
+      top: y * SQUARE_SIZE - TILE_OFFSET,
+      zIndex: `${x + 1}${y + 1}`,
+      opacity: isDragging ? 0.5 : 1
+    };
+
+    return connectDropTarget(
+      connectDragSource(
+        <div className="tile" style={styles}>
+          <span className="tile-letter">{letter}</span>
+          <span className="tile-points">{points}</span>
+        </div>
+      )
+    );
+  }
+}
+
+const squareTarget = {
+  drop(props, monitor) {
+    props.onDrop(props, monitor.getItem());
+  }
+};
+
+@DropTarget("tile", squareTarget, (connect, monitor) => ({
+  connectDropTarget: connect.dropTarget(),
+  isOver: monitor.isOver()
+}))
+class BoardSquare extends Component {
+  renderSquare() {
+    const classes = classNames({
+      "board-square": true,
+      "dragged-over": this.props.isOver
+    });
+
+    return <div className={classes} />;
+  }
+  render() {
+    if (this.props.tile) {
+      // If this square already has a tile in it, we don't want to allow drops.
+      return this.renderSquare();
+    } else {
+      return this.props.connectDropTarget(this.renderSquare());
+    }
+  }
+}
+
+export default Scrabble;
